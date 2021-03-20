d1cb1144de40c9430e1200778ebba9a4dfd807fb
ChickenRambutan <80835391+ChickenRambutan@users.noreply.github.com>
2021-03-20 18:59:31 +0800

    Parent class for MetaCritic and SteamGames

---
+++ kodingProject/src/kodingProject/Game.java
@@ -0,0 +1,31 @@
+
+public class Game {
+	private String gameTitle;
+	private String releaseDate;
+	private String developerStudio;
+	
+	public String getGameTitle() {
+		return gameTitle;
+	}
+
+	public void setGameTitle(String gameTitle) {
+		this.gameTitle = gameTitle;
+	}
+
+	public String getReleaseDate() {
+		return releaseDate;
+	}
+
+	public void setReleaseDate(String releaseDate) {
+		this.releaseDate = releaseDate;
+	}
+
+	public String getDeveloperStudio() {
+		return developerStudio;
+	}
+
+	public void setDeveloperStudio(String developerStudio) {
+		this.developerStudio = developerStudio;
+	}
+	
+}
