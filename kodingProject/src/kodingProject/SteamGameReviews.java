a70b567f7967389bd6c7b92eacf0ba07e417a2eb
botchance <80885804+botchance@users.noreply.github.com>
2021-03-20 16:36:58 +0800

    Add files via upload
    steam game/review class files

---
+++ kodingProject/src/kodingProject/SteamGameReviews.java
@@ -0,0 +1,74 @@
+import java.util.ArrayList;
+
+public class SteamGameReviews  {
+	private String review;
+	private String gameTitle;
+	private String gameReviewsDate;
+	private String foundHelpful;
+	private String recomendation;
+	private String gamehours;
+	
+	public SteamGameReviews() {
+		
+	}
+	public SteamGameReviews(String review, String gameTitle, String gameReviewsDate, String foundHelpful,
+			String recomendation, String gamehours) {
+		super();
+		this.gameTitle = gameTitle;
+		this.review = review;
+		this.gameReviewsDate = gameReviewsDate;
+		this.foundHelpful = foundHelpful;
+		this.recomendation = recomendation;
+		this.gamehours = gamehours;
+	}
+	
+	public String getGameTitle() {
+		return gameTitle;
+	}
+
+	public void setGameTitle(String gameTitle) {
+		this.gameTitle = gameTitle;
+	}
+	
+	public String getReview() {
+		return review;
+	}
+
+	public void setReview(String review) {
+		this.review = review;
+	}
+
+	public String getGameReviewsDate() {
+		return gameReviewsDate;
+	}
+
+	public void setGameReviewsDate(String gameReviewsDate) {
+		this.gameReviewsDate = gameReviewsDate;
+	}
+
+	public String getFoundHelpful() {
+		return foundHelpful;
+	}
+
+	public void setFoundHelpful(String foundHelpful) {
+		this.foundHelpful = foundHelpful;
+	}
+
+	public String getRecomendation() {
+		return recomendation;
+	}
+
+	public void setRecomendation(String recomendation) {
+		this.recomendation = recomendation;
+	}
+
+	public String getGamehours() {
+		return gamehours;
+	}
+
+	public void setGamehours(String gamehours) {
+		this.gamehours = gamehours;
+	}
+	
+	
+}
---
+++ kodingProject/src/kodingProject/SteamGames.java
@@ -0,0 +1,86 @@
+import java.util.ArrayList;
+
+public class SteamGames extends Game{
+	private int productID;
+	private String img;
+	private String desc;
+	private String sumReview;
+	private String releaseDate;
+	private String price;
+	private ArrayList<SteamGameReviews> listOfSteamReviews;
+
+	public SteamGames(int productID, String img, String desc, String sumReview, String releaseDate, String price,
+			ArrayList<SteamGameReviews> listOfSteamReviews) {
+		super();
+		this.productID = productID;
+		this.img = img;
+		this.desc = desc;
+		this.sumReview = sumReview;
+		this.releaseDate = releaseDate;
+		this.price = price;
+		this.listOfSteamReviews = listOfSteamReviews;
+	}
+
+	public SteamGames() {
+		
+	}
+
+	public int getProductID() {
+		return productID;
+	}
+
+	public void setProductID(int productID) {
+		this.productID = productID;
+	}
+
+	public String getImg() {
+		return img;
+	}
+
+	public void setImg(String img) {
+		this.img = img;
+	}
+
+	public String getDesc() {
+		return desc;
+	}
+
+	public void setDesc(String desc) {
+		this.desc = desc;
+	}
+
+	public String getSumReview() {
+		return sumReview;
+	}
+
+	public void setSumReview(String sumReview) {
+		this.sumReview = sumReview;
+	}
+
+	public String getReleaseDate() {
+		return releaseDate;
+	}
+
+	public void setReleaseDate(String releaseDate) {
+		this.releaseDate = releaseDate;
+	}
+
+	public String getPrice() {
+		return price;
+	}
+
+	public void setPrice(String price) {
+		this.price = price;
+	}
+
+	public ArrayList<SteamGameReviews> getListOfSteamReviews() {
+		return listOfSteamReviews;
+	}
+
+	public void setListOfSteamReviews(ArrayList<SteamGameReviews> listOfSteamReviews) {
+		this.listOfSteamReviews = listOfSteamReviews;
+	}
+
+	
+
+}
