376b70e8ac9165d0387c5f114c2054ffe72edfeb
botchance <80885804+botchance@users.noreply.github.com>
2021-03-20 16:46:12 +0800

    Crawler for steam games and reviews

--- kodingProject/src/kodingProject/SteamCrawler.java
+++ kodingProject/src/kodingProject/SteamCrawler.java
@@ -1,43 +1,28 @@
+import java.io.IOException;
+import java.net.SocketTimeoutException;
+import java.util.ArrayList;
+import java.util.Map;
+
 import org.jsoup.Connection;
-import org.jsoup.Connection.Method;
 import org.jsoup.Jsoup;
-import org.jsoup.helper.Validate;
+import org.jsoup.Connection.Method;
+import org.jsoup.HttpStatusException;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
-
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Map;
-import java.util.Scanner;
+import com.mongodb.client.MongoCollection;
 
 public class SteamCrawler {
 
-	public static void main(String[] args) throws IOException {
-		/*
-		ArrayList<String> userReviews = new ArrayList<>();
-
-		Document document = Jsoup.connect("https://www.metacritic.com/game/pc/hitman-3/user-reviews?dist=positive").get();
-		Elements reviewContent = document.select(".module.reviews_module.user_reviews_module");
-		for (Element reviews : reviewContent) {
-			System.out.print(reviews.select(".review_body span.blurb.blurb_expanded").isEmpty());
-			if (reviews.select(".review_body span.blurb.blurb_expanded").isEmpty()) {
-				userReviews.add(reviews.select(".review_body").text());
-			} else
-			userReviews.add(reviews.select(".review_body span.blurb.blurb_expanded").text());
-		}
-		System.out.println(userReviews);
-		*/
+	public SteamCrawler() {
 		
-		ArrayList<String> listOfGameLinks = getLinks();
-		getGameInfo(listOfGameLinks);
-
 	}
-
+	
 	public static ArrayList<String> getLinks() throws IOException {
-		String url = "https://store.steampowered.com/search/?category1=998&filter=topsellers";
-		Document document = Jsoup.connect(url).get();
+
+		final String url = "https://store.steampowered.com/search/?category1=998&filter=topsellers";
+		Document document = loadDocument(url);
 		Elements gameLinks = document.select("div#search_resultsRows > a");
 		ArrayList<String> links = new ArrayList<>();
 		
@@ -49,25 +34,20 @@ public class SteamCrawler {
 		return links;
 	}
 	
-	public static ArrayList<String> getGameInfo(ArrayList<String> listOfGameLinks) throws IOException {
-		ArrayList<String> gameTitle = new ArrayList<>();
-		ArrayList<String> gameImage = new ArrayList<>();
-		ArrayList<String> gameDesc = new ArrayList<>();
-		ArrayList<String> gameSumReview = new ArrayList<>();
-		ArrayList<String> gameReleaseDate = new ArrayList<>();
-		ArrayList<String> gamePrice = new ArrayList<>();
-		ArrayList<String> gameReviews = new ArrayList<>();
-		ArrayList<String> gameReviewsDate = new ArrayList<>();
-		ArrayList<String> foundHelpful = new ArrayList<>();
-		ArrayList<String> recomendation = new ArrayList<>();
-		ArrayList<String> gamehours = new ArrayList<>();
+	public static void getGameInfo(ArrayList<String> listOfGameLinks, MongoCollection<org.bson.Document> gameInfoCollection, MongoCollection<org.bson.Document> steamReviewCollection) throws IOException, HttpStatusException{
+		String gameTitle = null;
+		int productID = 0;
+		String ageCheckLink = "https://store.steampowered.com/agecheck/sub/504022";
 		
+		org.bson.Document steamStats = new org.bson.Document();
+        
 		String matureAge = "568022401";
 		String matureContent = "1";
 		
 		//bypass age check and mature content check
 		Connection.Response resp;
-		resp = Jsoup.connect("https://store.steampowered.com/agecheck/sub/504022")
+		
+		resp = Jsoup.connect(ageCheckLink)
         .userAgent("Mozilla/5.0")
         .timeout(10 * 1000)
         .method(Method.POST)
@@ -76,16 +56,13 @@ public class SteamCrawler {
         .data("ageYear", "1955") 
         .execute();
 		
-		//get cookies for mature content 
+		// get cookies for mature content 
 		final Map<String, String> mcookies = resp.cookies();
 		resp.cookies().put("birthtime", matureAge);
 		resp.cookies().put("mature_content", matureContent);
-		//print the cookies, we'll see birthtime and mature_content cookies here
-	    System.out.println(mcookies);
 	    
 		Connection.Response res;
-		
-		//retrieve game info
+		// retrieve game info
 		for (String link : listOfGameLinks) {
 			if(link.contains("https://store.steampowered.com/sub/")) {
 				continue;
@@ -94,57 +71,106 @@ public class SteamCrawler {
 					.cookies(mcookies)
 					.execute();
 		
+		// get steam product id
 		String[] stringArray = link.split("/");
-		String productID = stringArray[4];
+		productID = Integer.parseInt(stringArray[4]);
+		steamStats.append("productID", productID);
 		System.out.println(productID);
 
-			
 		//parse the document from response
 		Document dc = res.parse(); 
-		//get cookies
-
-		String title = dc.select("div.apphub_AppName").text();
-		gameTitle.add(title);
-		System.out.println(gameTitle); 
+		
+		// retrieve game info from steam
+		// get game title
+		gameTitle = dc.select("div.apphub_AppName").text();
+		steamStats.append("gameTitle", dc.select("div.apphub_AppName").text());
 
 		//gameTitle.add(dc.select("div.apphub_AppName").toString());
 		for (Element img : dc.select("div.game_header_image_ctn img")) {
-			gameImage.add(img.attr("src"));
+			steamStats.append("gameImage", img.attr("src"));
 		}
-		System.out.println(gameImage);
-		gameDesc.add(dc.select("div.game_description_snippet").text());
-		System.out.println(gameDesc);
+		
+		// get game description
+		steamStats.append("gameDesc", dc.select("div.game_description_snippet").text());
+		
+		//get game summary review
 		if (dc.select(".glance_ctn .user_reviews span.game_review_summary").isEmpty() != true) {
-			gameSumReview.add(dc.select(".glance_ctn .user_reviews span.game_review_summary").first().text());
+			steamStats.append("gameSumReview", dc.select(".glance_ctn .user_reviews span.game_review_summary").first().text());
 		} else 
-			gameSumReview.add("No user reviews");
-
-		System.out.println(gameSumReview);
-		gameReleaseDate.add(dc.select("div.date").text());
-		System.out.println(gameReleaseDate);
+			steamStats.append("gameSumReview", "No user reviews");
+		
+		// Get game release date
+		steamStats.append("gameReleaseDate", dc.select("div.date").text());
+		
+		// Get game developer
+		steamStats.append("developer", dc.getElementById("developers_list").text());
 		
 		if (dc.select("div.game_purchase_price").isEmpty() != true) {
-			gamePrice.add(dc.select("div.game_purchase_price").first().text());
+			steamStats.append("gamePrice", dc.select("div.game_purchase_price").first().text());
 		} else
-			gamePrice.add(dc.select("div.discount_final_price").first().text());
-		System.out.println(gamePrice);
+			steamStats.append("gamePrice", dc.select("div.discount_final_price").first().text());
 		
-		//retrieve reviews 
+		System.out.println(steamStats);
+		gameInfoCollection.insertOne(steamStats);
+		steamStats.clear();
+		System.out.println("empty?" +steamStats);
+		
+		// retrieve reviews from steam
+		org.bson.Document steamReview = new org.bson.Document();
 		String reviewLink = "http://steamcommunity.com/app/" + productID + "/reviews/?browsefilter=trendmonth&p=1";
 		Document revDoc = Jsoup.connect(reviewLink).get();
-		Elements revEss  = revDoc.select(".apphub_Card");
-		for (Element reviews : revEss) {
-			foundHelpful.add(reviews.select(".found_helpful").text());
-			recomendation.add(reviews.select(".vote_header .title").text());
-			gamehours.add(reviews.select(".vote_header .hours").text());
-			gameReviewsDate.add(reviews.select(".apphub_CardTextContent .date_posted").text());
-			gameReviews.add(reviews.select(".apphub_CardTextContent").first().ownText());
+		
+		if (revDoc.select(".apphub_CardContentMain").isEmpty() == false){
+			Elements revEss  = revDoc.select(".apphub_CardContentMain");
+			for (Element reviews : revEss.select(".apphub_UserReviewCardContent")) {
+				
+				steamReview.append("gameTitle", gameTitle);
+				steamReview.append("gameReview", (reviews.select(".apphub_CardTextContent").first().ownText()));
+				steamReview.append("foundHelpful", (reviews.select(".found_helpful").first().childNodes().get(0).toString().stripLeading()));
+				steamReview.append("recomendation", (reviews.select(".vote_header .title").text()));
+				steamReview.append("gamehours", (reviews.select(".vote_header .hours").text()));
+				steamReview.append("gameReviewsDate", (reviews.select(".apphub_CardTextContent .date_posted").text()));
+				
+				System.out.println(steamReview);
+				steamReviewCollection.insertOne(steamReview);
+				steamReview.clear();
+			}
+		} else {
+			System.out.println("No reviews Availble");
+			steamReview.append("gameTitle", gameTitle);
+			steamReview.append("gameReview", "No reviews yet");
+			steamReview.append("foundHelpful", "Null");
+			steamReview.append("recomendation", "Null");
+			steamReview.append("gamehours", "Null");
+			steamReview.append("gameReviewsDate", "Null");
+			
+			System.out.println(steamReview);
+			steamReviewCollection.insertOne(steamReview);
+			steamReview.clear();
 		}
-		System.out.println(gameReviews);
+		
 	 }
-		return gameTitle;
 	}
 	
-
+	//this method is to load document, if exception caught retry and fetch again
+		public static Document loadDocument(String url) throws IOException {
+			Document load;
+			//if url caught by an exception, connect and get the url again
+			do {
+				try {
+					load = Jsoup.connect(url).get();
+				} catch (HttpStatusException e) {
+					System.out.println("HttpStatus error fetching: " + url);
+					System.out.println("Retrying to fetch..." + url);
+					continue;
+				}
+				catch (SocketTimeoutException e) {
+					System.out.println("Timeout fetching: " + url);
+					System.out.println("Retrying to fetch..." + url);
+					continue;
+				}
+				break; //break the while loop, if connect is successful
+			} while (true);
+			return load;
+		}
 }
-
